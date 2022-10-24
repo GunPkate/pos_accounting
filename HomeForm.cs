e89823ffdd97ce6126b3d952592077e123e9a5fe
GunPKate <gun@builk.com>
2022-10-24 11:33:57 +0700

    Add project files.

---
+++ HomeForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSAccount
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
